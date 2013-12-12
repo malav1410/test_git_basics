3c39d0dc0b4f80d21f084b2642e212f6601ddd78
Malav Desai <malav1410@gmail.com>
2013-12-12 16:16:32 +0530

    Third commit

--- hello.rb
+++ hello.rb
@@ -1,2 +1,17 @@
-puts "hello world"
-puts "hello world"
+FIRST_COMMIT = { changes: "puts hello world" }
+
+MASTER = FIRST_COMMIT
+HEAD = FIRST_COMMIT
+
+puts "MASTER = #{MASTER.to_s}"
+puts "HEAD = #{HEAD.to_s}"
+
+SECOND_COMMIT = { changes: "puts hello world: line 2" }
+
+MASTER = SECOND_COMMIT
+HEAD = SECOND_COMMIT
+
+puts "MASTER = #{MASTER.to_s}"
+puts "HEAD = #{HEAD.to_s}"
+puts "FIRST_COMMIT = #{FIRST_COMMIT.to_s}"
+puts "SECOND_COMMIT = #{SECOND_COMMIT.to_s}"
